cc586e436727132e6942f2df13749ef823d41507
Dzhanbolot Abdrasilov <90703397+ZhanbolotAbdrasilov17@users.noreply.github.com>
2024-05-19 17:16:35 +0600

    Add files via upload

---
+++ lesson2_CuKsmEY.py
@@ -0,0 +1,80 @@
+
+# Mutable(изменяемые типы данных)
+# 1. list
+# 2. set
+# 3. dict
+
+# Immutable(неизменяемые типы данных)
+# 1. None
+# 2. bool
+# 3. int, float, complex
+# 4. str
+# 5. tuple, range
+# 6. frozenset
+
+
+# = -> оператор присваивания
+# variable(переменная)
+
+# num = 5
+# num = 6
+# print(num)
+
+# Код на языке Python читается сверху вниз,
+# слева направо!
+
+# abc - нижний регистр
+# ABC - верхний регистр
+
+# PEP8 - правило написания кода на языке пайтон
+
+# tomorrow_day = '23.10.2023'
+# tomorrowDay = '23.10.2023'
+
+
+# num1 = 10
+# num2 = 10
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+
+# num1 = 5
+# num2 = 10
+# print('Result: ', num1, '+', num2, '=', num1 + num2)
+
+
+# ----------------------------------------------------
+
+# name = input("Enter your name: ")
+# surname = input("Enter your surname: ")
+# age = input("Enter your age: ")
+# print("Hello", name, surname, '!', 'You are', 'age', age)
+
+
+# input - функция ввода значения от пользователя
+
+
+# Операторы
+# + это сложение
+# - это вычитание
+# * это умножение
+# / это обычное деление
+# // это деление без остатка
+# % это получение остатка от деления
+
+# num1 = 20
+# num2 = 6
+# print('/', num1 / num2)
+# print('//', num1 // num2)
+# print('%', num1 % num2)
+
+# print(5 ** 2)
+# print(5 ** 3)
+
+# ** это возведение в степень
+
+# print(9 ** 0.5) это нахождение квадратного корня
+
+
+print(pow(5, 3))
---
+++ lesson3_nBXEO8P.py
@@ -0,0 +1,80 @@
+# 'String' - Строка
+# "Hello world! My name is Albert Einstein"
+
+# str1 = """
+# I'm Kylian Mbappe!
+# I'm captain of France!
+# I'm 25 years old!
+# """
+
+# print(str1)
+
+# Строка - это тип данных используется для
+# хранения и представления текстовой информации.
+
+
+# Индексация в строке
+
+name = 'Sergio'
+        # S = 0 -6
+        # e = 1 -5
+        # r = 2 -4
+        # g = 3 -3
+        # i = 4 -2
+        # o = 5 -1
+
+# print(name)
+# # print(name[0])
+# # print(name[1])
+# # print(name[2])
+# # print(name[3])
+# # print(name[4])
+# # print(name[5])
+# print(name[-1])
+
+# Срезы по индексам
+# [start: end: <step>]
+# str1 = 'Codifylab'
+# print(str1[0:9:3])
+# ------------------------------------
+
+# Конкатенация строк (соединение) +
+
+# a = 'Hello,'
+# c = ' '
+# b = 'World!'
+# print(a + c + b)
+
+# a = 'Hello,'
+# c = ' '
+# b = 'World!'
+# res = a + c + b
+# print(res)
+
+# a = 'abc'
+# print(a * 5)
+
+
+# \n -> перенос строки
+# \t -> горизональная табуляция
+
+# str1 = "Hello, World!"
+# print(str1)
+
+# str2 = "Hello, World!\nHello John!"
+# print(str2)
+
+
+# ----------------------------
+# Форматирование строк
+# name = input("Введите имя: ")
+# last_name = input("Введите фамилию: ")
+# # print(name, last_name)
+# print(f"Hello mr/mrs {name}, {last_name}! Welcome to the party!")
+
+# -----------------------------------------------------------
+
+# Метод это функция, к-рая принадлежит к контректному классу
+# и не может быть использована отдельно от него.
+
+print(dir(str))
